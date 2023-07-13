1d2548a16ce4c7ed738808c60f72f488096398a6
giannipiccardo <giannipiccardo@gmail.com>
2023-07-13 11:49:05 -0300

    13-07 11:49

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import PokemonCard from "./Components/PokemonCard"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const data = await res.json();
+
+      setPokemons(data.results);
+    };
+
+    getPokemons();
+  }, []);
+  console.log(pokemons);
 
   return (
-    <>
-    </>
-  )
+    <div>
+      {pokemons.length ? <PokemonCard url={pokemons[0].url}></PokemonCard> : "Cargando contenido..."}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/PokemonCard.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+function PokemonCard({ url }) {
+    const [pokemon, setPokemon] = useState();
+
+    useEffect(() => {
+        const getPokemon = async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+
+            setPokemon(data);
+        };
+
+        getPokemon();
+    }, []);
+    console.log(pokemon);
+    return (
+        pokemon ? <div>{pokemon.name}</div> : <h1>Loading pokemon</h1>
+    );
+}
+
+export default PokemonCard;
