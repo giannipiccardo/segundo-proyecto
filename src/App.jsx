0e7416dc9668ed491875458e48d2b75e3882a928
giannipiccardo <giannipiccardo@gmail.com>
2023-07-20 09:44:17 -0300

    rama1 primer cambio

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
       <div className="home-title">
         <div className="pokedex">
           <img src={Logo} alt="Logo" />
-          <span>Pokédex v1</span>
+          <span>Pokédex rama1</span>
         </div>
         <button onClick={() => filtrar()} className="home-filters">
           <span>{boton}</span>
